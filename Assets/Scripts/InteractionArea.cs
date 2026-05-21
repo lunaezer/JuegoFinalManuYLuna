8920cb57d885e16fe109f241de07273e581548af
lunaezer <lunaezer22@gmail.com>
2026-05-21 11:56:51 -0300

    score-txt

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class InteractionArea : MonoBehaviour
 {
+    public int score = 0;
+    public UIManager uiManagerScript;
+
+    void Awake()
+    {
+        uiManagerScript = GameObject.FindObjectOfType<UIManager>();
+    }
+
   void OnTriggerEnter(Collider other)
   {
     if(other.gameObject.CompareTag("Collectible")){
         Destroy(other.gameObject);
+        score ++;
+        uiManagerScript.UpdateScore(score);
     }
   }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //poner el score en cero
+        UpdateScore(0);
+    }
+
+   public void UpdateScore(int score)
+   {
+        txtScore.text = "Score: " + score.ToString();   //actualizar el texto score
+   }
+}
